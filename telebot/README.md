cac1ca2504ccbcd36c2e45cf28aad9ca00b97d2f
Swapnil Jain <swapnil@linux.com>
2022-04-24 10:16:26 +1000

    lambda function for telegram bot

---
+++ telebot/README.md
@@ -0,0 +1,3 @@
+# Telegram bot for realtime updates from 3Commas
+
+Telegram Client --> Telegram API --> Webhook -> AWS API Gateway --> Lambda Function
---
+++ telebot/lambda_function.py
@@ -0,0 +1,72 @@
+import os
+import json
+import requests
+import sys
+from py3cw.request import Py3CW
+
+TELE_TOKEN = os.environ['TELE_TOKEN']
+URL = "https://api.telegram.org/bot{}/".format(TELE_TOKEN)
+
+def getuid(username):
+    accountid = os.environ[username]
+    return accountid
+
+p3cw = Py3CW(
+    key=os.environ['TC_KEY'],
+    secret=os.environ['TC_SECRET'],
+    request_options={
+        'request_timeout': 10,
+        'nr_of_retries': 1,
+        'retry_status_codes': [502],
+        'retry_backoff_factor': 0.1
+    }
+)
+
+def send_message(text, chat_id):
+    url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
+    requests.get(url)
+
+def start(arguments, chat_id):
+    """Send a message when the command /start is issued."""
+    message = "Hello " + arguments
+    send_message(message, chat_id)
+    send_message('/activedeals - Current Active Deals\n/closeddeals - Last 5 Closed Deals\n/overallstats - Overall Stats',chat_id)
+
+def overallstats(accountid, chat_id):
+
+    error, bots_data = p3cw.request(
+        entity = 'bots',
+        action = 'stats',
+        payload={
+        'account_id': accountid,
+        }
+    )
+
+    profits = bots_data['profits_in_usd']
+    for key, value in profits.items():
+        amount = "${:,.2f}".format(value)
+        send_message(key + ": " + amount, chat_id)
+
+
+def lambda_handler(event, context):
+    chat_id = event['message']['chat']['id']
+    fname = event['message']['chat']['first_name']
+    username = event['message']['chat']['username']
+    accountid = getuid(username)
+    command_arguments = event['message']['text'].split()
+    command = command_arguments[0]
+    arguments = command_arguments[1:]
+    if command == "/start":
+        if accountid != "invalid":
+            start(fname, chat_id)
+        else:
+            send_message('Hi ' + str(user.first_name) + ' you are not authorised to use this service',chat_id)
+    elif command == "/overallstats":
+            overallstats(accountid,chat_id)
+    else:
+        send_message("Command not support", chat_id)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Bot Stopped')
+    }
